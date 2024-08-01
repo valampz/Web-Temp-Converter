c7d270b72e6988289770dff87dd92a89b32082ff
Tazim Chowdhury <tazimezazchy@gmail.com>
2024-08-02 05:56:16 +0600

    Add script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const textBox = document.getElementById("textBox")
+const toFahrenheit = document.getElementById("toFahrenheit")
+const toCelsius = document.getElementById("toCelsius")
+const result = document.getElementById("result")
+let temp
+
+function convert(){
+    if(toFahrenheit.checked){
+        let c = Number(textBox.value)
+        temp = (c * 9/5) + 32
+        result.textContent = temp + "°F"
+    }
+    else if (toCelsius.checked){
+        let f = Number(textBox.value)
+        temp = (f - 32) * 5/9
+        result.textContent = temp + "°C"
+    }
+    else{
+        result.textContent = "Select an Unit"
+    }
+}
